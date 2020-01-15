55d766ca2f60019f49ad0687bebe58b5ea743d0e
Jun <sakakibarajun4@gmail.com>
2020-01-15 15:15:23 -0300

    Updates

---
+++ precache-manifest.e9d3f40dc496686896680155f476deda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1571319ea8eaee83d42c13af5515138",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "901172eaa08dd470bc57",
+    "url": "/monster-rolodex/static/css/main.fb6390be.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "901172eaa08dd470bc57",
+    "url": "/monster-rolodex/static/js/main.0a804ad0.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
